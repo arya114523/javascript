6b60be3772985441708c162d58e4cc22a3e4ab48
Arya Padole <aryapadole523@gmail.com>
2025-08-29 18:41:33 +0530

    Switch

---
+++ Switch.js
@@ -0,0 +1,15 @@
+let color = "pink";
+
+switch (color) {
+    case "red":
+        console.log("Stop! Light is red.");
+        break;
+    case "yellow":
+        console.log("Slow down. Light is yellow.");
+        break;
+    case "green":
+        console.log("Go! Light is green.");
+        break;
+    default:
+        console.log("Invalid color.");
+}
