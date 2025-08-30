c412943f6b787ac10820e38e8d4a03f68b2bf0f9
Arya Padole <aryapadole523@gmail.com>
2025-08-30 19:09:52 +0530

    methodechaining

--- Stringop.js
+++ Stringop.js
@@ -2,3 +2,5 @@ let name = "AryaPadole";
 console.log(name.slice(4,9));
 console.log(name.indexOf("Ar"));
 console.log(name.replace("Arya","Ankita"));
+console.log(name.slice(4));
+console.log(name.slice(4).replace('l','t'));
